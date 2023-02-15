6031d02e6098f23841ed2cd190ac864feff3b440
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-02-15 14:51:26 -0600

    added buttons, input fields, framework to add on to

---
+++ client.js
@@ -0,0 +1,36 @@
+console.log("Let's crunch some numbers.");
+
+
+function submitForm(event) {
+    console.log('submitForm');
+    event.preventDefault(); // Stop the page from refreshing as default
+
+
+    //! .value always returns a string, for a number use Number(string)
+
+    let nameFirstVal = document.querySelector('#nameFirst-input').value;
+    let nameLastVal = document.querySelector('#nameLast-input').value;
+    let idVal = document.querySelector('#id-input').value
+    let titleVal = document.querySelector('#jobTitle-input').value;
+    let salaryVal= document.querySelector('#salary-input').Number(String); //testing number(String)
+    let infoTable = document.querySelector('#infoData');
+    infoTable.innerHTML += `
+        <tr>
+            <td>${nameFirstVal}</td>
+            <td>${nameLastVal}</td>
+            <td>${idVal} </td>
+            <td>${titleVal}</td>
+            <td>${salaryVal}</td>
+
+            <td button onClick="removeRow(event)"> Click to delete </td>
+            </tr>
+    `;
+   
+
+
+}//end
+
+function removeRow(event) { //start
+    event.target.parentElement.remove(); //removes parent element (whole music note generated from button)
+    } //end
+    
