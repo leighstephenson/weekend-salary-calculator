6f2b662606708491c1b9cfd7a78bfbc987ead1f5
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-02-18 11:49:29 -0600

    got monthly cost stored in array and ready to find the sum

--- client.js
+++ client.js
@@ -2,13 +2,13 @@
 
 let totalMonthly = 0; 
 let costArray = [];
+let salaryInput = document.querySelector('#salary-input');
+
 
 
 function submitForm(event) {//start
     //console.log('Submission function works'); //test
     event.preventDefault() 
-    let userInput = document.querySelector(".userInput"); //sets var to = userInput class on HTML
-    userInput.reset(); //makes input form reset
     
     //! .value always returns a string, for a number use Number(document.querySelector('#linkID').value);
     let nameFirstVal = document.querySelector('#nameFirst-input').value;
@@ -18,9 +18,11 @@ function submitForm(event) {//start
     let salaryVal= Number(document.querySelector('#salary-input').value); 
     let infoTable = document.querySelector('#infoData');
     
-
+    let userInput = document.querySelector(".userInput"); //sets var to = userInput class on HTML
+    userInput.reset(); //makes input form reset
+    
     infoTable.innerHTML += `
-        <tr>
+    <tr>
             <td>${nameFirstVal}</td>
             <td>${nameLastVal}</td>
             <td>${idVal} </td>
@@ -28,11 +30,19 @@ function submitForm(event) {//start
             <td>${salaryVal}</td>
 
             <td button onClick="removeRow(event)"> Click to delete </td>
-            </tr>
-    `
+     </tr>
+    `;
+    let totalMonthly = salaryVal / 12; //divides annual salary by 12 to get monthly cost
+    costArray.push(totalMonthly); //pushes the salary input into array
+    console.log(costArray); //console logs the new array 
+  
+    
 };//end
+
 
 function removeRow(event) { //start
     event.target.parentElement.remove(); //removes parent element (whole music note generated from button)
     }; //end
+
+
 
