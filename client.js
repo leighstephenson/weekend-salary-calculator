fdec2321012c0e3148b7c3674653c97a078a43fb
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-02-17 23:37:37 -0600

    Added form reset on submit and setting up to find monthly cost

--- client.js
+++ client.js
@@ -1,19 +1,24 @@
-console.log("Let's crunch some numbers.");
+//console.log("Let's crunch some numbers."); //test
+
+let totalMonthly = 0; 
+let costArray = [];
 
 
-function submitForm(event) {
-    console.log('submitForm');
-    event.preventDefault(); // Stop the page from refreshing as default
-
-
-    //! .value always returns a string, for a number use Number(string)
-
+function submitForm(event) {//start
+    //console.log('Submission function works'); //test
+    event.preventDefault() 
+    let userInput = document.querySelector(".userInput"); //sets var to = userInput class on HTML
+    userInput.reset(); //makes input form reset
+    
+    //! .value always returns a string, for a number use Number(document.querySelector('#linkID').value);
     let nameFirstVal = document.querySelector('#nameFirst-input').value;
     let nameLastVal = document.querySelector('#nameLast-input').value;
-    let idVal = document.querySelector('#id-input').value
+    let idVal = Number(document.querySelector('#id-input').value);           //all variables link to IDs in HTML
     let titleVal = document.querySelector('#jobTitle-input').value;
-    let salaryVal= document.querySelector('#salary-input').value; 
+    let salaryVal= Number(document.querySelector('#salary-input').value); 
     let infoTable = document.querySelector('#infoData');
+    
+
     infoTable.innerHTML += `
         <tr>
             <td>${nameFirstVal}</td>
@@ -24,11 +29,10 @@ function submitForm(event) {
 
             <td button onClick="removeRow(event)"> Click to delete </td>
             </tr>
-    `;
-
-}//end
+    `
+};//end
 
 function removeRow(event) { //start
     event.target.parentElement.remove(); //removes parent element (whole music note generated from button)
-    } //end
-    
+    }; //end
+
