4b0e78c8593926ff830a2be2727251231e97fa23
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-02-18 17:25:16 -0600

    Got monthly cost per employee displaying in the table, added simple CSS to make this easier to read and test

--- client.js
+++ client.js
@@ -1,8 +1,9 @@
-//console.log("Let's crunch some numbers."); //test
+console.log("Let's crunch some numbers."); //test
 
 let totalMonthly = 0; 
 let costArray = [];
 let salaryInput = document.querySelector('#salary-input');
+let sum = 0;
 
 
 
@@ -10,14 +11,14 @@ function submitForm(event) {//start
     //console.log('Submission function works'); //test
     event.preventDefault() 
     
-    //! .value always returns a string, for a number use Number(document.querySelector('#linkID').value);
     let nameFirstVal = document.querySelector('#nameFirst-input').value;
     let nameLastVal = document.querySelector('#nameLast-input').value;
     let idVal = Number(document.querySelector('#id-input').value);           //all variables link to IDs in HTML
     let titleVal = document.querySelector('#jobTitle-input').value;
     let salaryVal= Number(document.querySelector('#salary-input').value); 
     let infoTable = document.querySelector('#infoData');
-    
+    let monthlyCost = salaryVal / 12; //divides annual salary by 12 to get monthly cost
+
     let userInput = document.querySelector(".userInput"); //sets var to = userInput class on HTML
     userInput.reset(); //makes input form reset
     
@@ -28,21 +29,29 @@ function submitForm(event) {//start
             <td>${idVal} </td>
             <td>${titleVal}</td>
             <td>${salaryVal}</td>
-
-            <td button onClick="removeRow(event)"> Click to delete </td>
-     </tr>
+            <td>${monthlyCost}</td>
+            <td button onClick="removeRow(event)"> Delete </td>
+    </tr>
     `;
-    let totalMonthly = salaryVal / 12; //divides annual salary by 12 to get monthly cost
-    costArray.push(totalMonthly); //pushes the salary input into array
+
+    costArray.push(monthlyCost); //pushes the salary input into array
     console.log(costArray); //console logs the new array 
   
-    
-};//end
+
+
+};//end submitForm() function
+
+//!find the sum- testing, troubleshooting
+// costArray.forEach(item => {
+//     sum += item; 
+//     console.log('Current sum', sum)
+// });
+//for (let i = 0; i < costArray.length; i++) {
+//    sum += costArray[i];
+//  }
+
 
 
 function removeRow(event) { //start
     event.target.parentElement.remove(); //removes parent element (whole music note generated from button)
     }; //end
-
-
-
