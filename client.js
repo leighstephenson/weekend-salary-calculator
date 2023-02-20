029ebc09aa70b6a85b1506d88b56d31b2172f717
Leigh Stephenson <Leighh.stephenson@gmail.com>
2023-02-20 14:26:38 -0600

    fixed sum, changed a few things on the table to make total monthly cost display with other values

--- client.js
+++ client.js
@@ -1,15 +1,14 @@
 console.log("Let's crunch some numbers."); //test
 
-let totalMonthly = 0; 
 let costArray = [];
-let salaryInput = document.querySelector('#salary-input');
+let totalMonthly = 0; //used for adding costs of employee salary inputs
+let maxMonthly = 20000 
 let sum = 0;
-
 
 
 function submitForm(event) {//start
     //console.log('Submission function works'); //test
-    event.preventDefault() 
+    event.preventDefault(); //prevents default refreshing 
     
     let nameFirstVal = document.querySelector('#nameFirst-input').value;
     let nameLastVal = document.querySelector('#nameLast-input').value;
@@ -18,11 +17,22 @@ function submitForm(event) {//start
     let salaryVal= Number(document.querySelector('#salary-input').value); 
     let infoTable = document.querySelector('#infoData');
     let monthlyCost = salaryVal / 12; //divides annual salary by 12 to get monthly cost
+    //let totalMonthlyCost = document.querySelector('#totalMonthlyCost')
 
     let userInput = document.querySelector(".userInput"); //sets var to = userInput class on HTML
     userInput.reset(); //makes input form reset
-    
-    infoTable.innerHTML += `
+
+//! Push monthly cost into array
+costArray.push(monthlyCost); //pushes the salary input / 12 into array
+console.log('New Array from input data:', costArray); //console logs the new array 
+
+
+//! Tring to find the sum of array 
+    sum += monthlyCost //adds monthly cost to sum
+    console.log("Current sum:",sum); //displays sum
+
+   
+      infoTable.innerHTML += `
     <tr>
             <td>${nameFirstVal}</td>
             <td>${nameLastVal}</td>
@@ -30,28 +40,29 @@ function submitForm(event) {//start
             <td>${titleVal}</td>
             <td>${salaryVal}</td>
             <td>${monthlyCost}</td>
+            <td>${sum} </td>
             <td button onClick="removeRow(event)"> Delete </td>
     </tr>
     `;
 
-    costArray.push(monthlyCost); //pushes the salary input into array
-    console.log(costArray); //console logs the new array 
-  
+};//end submitForm() function 
 
-
-};//end submitForm() function
-
-//!find the sum- testing, troubleshooting
-// costArray.forEach(item => {
-//     sum += item; 
-//     console.log('Current sum', sum)
-// });
-//for (let i = 0; i < costArray.length; i++) {
-//    sum += costArray[i];
-//  }
-
+  // //! Changing background color when over budget
+//if (sum >= maxMonthly) { //start
 
 
 function removeRow(event) { //start
     event.target.parentElement.remove(); //removes parent element (whole music note generated from button)
     }; //end
+
+
+
+
+
+
+
+
+  
+
+
+
